b8547e92e6a4ff6f356db59148eb8bbb1a74ce82
Benjamin Pope <buddypope1234@gmail.com>
2022-10-23 13:10:42 -0500

    Adding styles to Rest-rant

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Expres settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
