0c31cc1de4910573d1ac1767d7fbf9c33fcc50a3
Benjamin Pope <buddypope1234@gmail.com>
2022-10-25 21:41:09 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -5,6 +5,23 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
 
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+  res.render('places/show', { place: places[id]})
+  }
+})
+
 router.post('/', (req, res) => {
   if (!req.body.pic) {
     //Default image if on is not provided
@@ -20,9 +37,6 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
 
 
 
